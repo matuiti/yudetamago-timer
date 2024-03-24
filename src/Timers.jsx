6c9255f13248260734c3ff07726d3a30fb0ed240
matuiti <ichi.animo@gmail.com>
2024-03-24 12:35:42 +0900

    Katayude.jsxを実装

--- src/Timers.jsx
+++ src/Timers.jsx
@@ -1,16 +1,17 @@
-import { useState, useEffect } from "react"
+// import { useState, useEffect } from "react"
+import Katayude from "./components/Katayude"
 
 const Timers = () => {
   // 初期値として各種ゆで方の時間を設定します（秒数で表記）
   const initialTimers = {
-    hardBoiled: 720, // 硬ゆで（12分）
-    softBoiled: 480, // 半熟  （8分）
-    poached:    720, // 温泉卵（12分）
+    katayude: 720, // 硬ゆで（12分）
+    hannjuku: 480, // 半熟  （8分）
+    onnsenn:  720, // 温泉卵（12分）
   }
 
-  const [timer, setTimer] = useState(initialTimers.hardBoiled)
-  const [isRunning, setIsRunning] = useState(false)
-  const [isStartButtonVisible, setIsStartButtonVisible] = useState(true)
+  // const [timer, setTimer] = useState(initialTimers.katayude)
+  // const [isRunning, setIsRunning] = useState(false)
+  // const [isStartButtonVisible, setIsStartButtonVisible] = useState(true)
   const zeroPad = (num) => (num < 10 ? "0" + num : num)
 
   // タイマーの表示形式を作成する関数
@@ -21,87 +22,45 @@ const Timers = () => {
   }
 
   // カウントダウンロジックを実装する関数
-  const countdown = () => {
-    if (timer > 0) {
-      setTimer(timer - 1)
-    } else {
-      // タイマーが0になったら音を鳴らしてカウントを初期値に戻す
-      new Audio("/bell.mp3").play()
-      setTimer(initialTimers.hardBoiled)
-      setIsRunning(false)
-      setIsStartButtonVisible(true) // リセット時にスタートボタンを表示
-    }
-  }
+  // const countdown = () => {
+  //   if (timer > 0) {
+  //     setTimer(timer - 1)
+  //   } else {
+  //     // タイマーが0になったら音を鳴らしてカウントを初期値に戻す
+  //     new Audio("/bell.mp3").play()
+  //     setTimer(initialTimers.katayude)
+  //     setIsRunning(false)
+  //     setIsStartButtonVisible(true) // リセット時にスタートボタンを表示
+  //   }
+  // }
 
   // タイマーが動作中の場合、1秒ごとにカウントダウン関数を呼び出す
-  useEffect(() => {
-    let timerId
-    if (isRunning) {
-      timerId = setInterval(countdown, 1000)
-      setIsStartButtonVisible(false) // タイマーが開始されたらスタートボタンを非表示
-    } else {
-      clearInterval(timerId)
-    }
-    return () => clearInterval(timerId)
-  }, [isRunning, timer])
+  // useEffect(() => {
+  //   let timerId
+  //   if (isRunning) {
+  //     timerId = setInterval(countdown, 1000)
+  //     setIsStartButtonVisible(false) // タイマーが開始されたらスタートボタンを非表示
+  //   } else {
+  //     clearInterval(timerId)
+  //   }
+  //   return () => clearInterval(timerId)
+  // }, [isRunning, timer])
 
   // スタート/リセットボタンを切り替える関数
-  const toggleButton = () => {
-    if (!isRunning) {
-      setIsRunning(true)
-    } else {
-      setTimer(initialTimers.hardBoiled)
-      setIsRunning(false)
-      setIsStartButtonVisible(true) // リセット時にスタートボタンを表示
-    }
-  }
+  // const toggleButton = (initialTime) => {
+  //   if (!isRunning) {
+  //     setIsRunning(true)
+  //   } else {
+  //     setTimer(initialTime)
+  //     setIsRunning(false)
+  //     setIsStartButtonVisible(true) // リセット時にスタートボタンを表示
+  //   }
+  // }
 
   return (
     <ul style={{ display: "flex", flexDirection: "column",rowGap:"36px",padding:"63px 8px"}}>
-      <li style={{ display: "flex", justifyContent:"space-between"}}>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            width:"81px"
-          }}
-        >
-          <img
-            src="public\images\katayude@2x.png"
-            style={{ width: "65px", height: "79px" }}
-          />
-        </div>
-        <div
-          style={{
-            color: "#492C15",
-            fontSize: "56px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          {formatTime(timer)}
-        </div>
-        <div style={{ display: "flex" }}>
-          <button
-            style={{
-              margin: "auto",
-              width: "75px",
-              height: "75px",
-              backgroundColor: "#FF774E",
-              borderRadius: "50%",
-              fontSize: "14px",
-              color: "#FFFFFF",
-            }}
-            onClick={toggleButton}
-            disabled={timer <= 0 && !isRunning}
-          >
-            {isStartButtonVisible ? "スタート" : "リセット"}
-          </button>
-        </div>
-      </li>
-      <li style={{ display: "flex", justifyContent:"space-between"}}>
+      <Katayude initialTime={initialTimers.katayude} formatTime={formatTime} />
+      {/* <li style={{ display: "flex", justifyContent:"space-between"}}>
         <div
           style={{
             display: "flex",
@@ -185,7 +144,7 @@ const Timers = () => {
             {isStartButtonVisible ? "スタート" : "リセット"}
           </button>
         </div>
-      </li>
+      </li> */}
     </ul>
   )
 }
